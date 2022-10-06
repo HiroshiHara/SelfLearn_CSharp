4ccd2206ac528f62c96fe8f660cab6f6b072593d
HiroshiHara <mrbob403@gmail.com>
2022-10-06 22:55:14 +0900

    progress

---
+++ SelfCSharp8_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SelfCSharp8_1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ SelfCSharp8_1/Triangle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfCSharp8_1
+{
+    internal class Triangle
+    {
+        private double width;
+        private double height;
+
+        // getter, setter
+        public double GetWidth()
+        {
+            return this.width;
+        }
+
+        public double GetHeight()
+        {
+            return this.height;
+        }
+
+        public void SetWidth(double width)
+        {
+            if (width <= 0)
+            {
+                throw new ArgumentException("正数を指定してください。");
+            }
+            this.width = width;
+        }
+
+        public void SetHeight(double height)
+        {
+            if (height <= 0)
+            {
+                throw new ArgumentException("正数を指定してください。");
+            }
+            this.height = height;
+        }
+    }
+}

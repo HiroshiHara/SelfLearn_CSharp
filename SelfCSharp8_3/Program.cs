39ba30db5d5eb2262c5bbb27242329590a0ffde7
HiroshiHara <mrbob403@gmail.com>
2022-10-07 19:57:17 +0900

    progress

---
+++ SelfCSharp8_3/Program.cs
@@ -0,0 +1,14 @@
+﻿// 拡張メソッド使用にはそのメソッドの名前空間を指定する必要あり
+// この例は同じnamespaceに存在するので指定不要
+
+namespace SelfCSharp8_3
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string repeat = "repeat";
+            Console.WriteLine(repeat.Repeat(20));
+        }
+    }
+}
---
+++ SelfCSharp8_3/StringExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfCSharp8_3
+{
+    // 拡張メソッド実装クラスはstaticである必要あり
+    // ※staticクラス→インスタンス化禁止
+    internal static class StringExtensions
+    {
+        // 拡張メソッド
+        // 継承せず既存のクラスのメソッドを拡張する
+        // 継承とは異なる仕組みの為sealedクラスにも有効
+        // 条件
+        // ・拡張元がstaticクラスのstaticメソッドであること
+        // ・拡張先で第一引数にthisキーワードで拡張元クラス自身を指定すること
+        public static string Repeat(this string str, int count)
+        {
+            // str.Repeat(int)で実行される
+            // 第一引数が拡張メソッドの構文規則兼実際の操作対象
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < count; i++)
+            {
+                sb.Append(str);
+            }
+            return sb.ToString();
+        }
+    }
+}

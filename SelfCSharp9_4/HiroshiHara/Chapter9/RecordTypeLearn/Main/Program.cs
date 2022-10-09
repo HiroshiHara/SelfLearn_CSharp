776ed6f29964b02998dbdf91c77178f4f6794071
HiroshiHara <mrbob403@gmail.com>
2022-10-09 20:06:50 +0900

    progress

---
+++ SelfCSharp9_4/HiroshiHara/Chapter9/RecordTypeLearn/Main/Program.cs
@@ -0,0 +1,26 @@
+﻿using SelfCSharp9_4.HiroshiHara.Chapter9.RecordTypeLearn.Record;
+
+namespace SelfCSharp9_4.HiroshiHara.Chapter9.RecordTypeLearn.Main
+{
+    internal class Program
+    {
+        public static void Main(string[] args) {
+            // レコード型利用のサンプル
+            Person p1 = new Person("山田", "太郎", 22);
+            Person p2 = new Person("田中", "二郎", 29);
+            Person p3 = new Person("山田", "太郎", 22);
+            Console.WriteLine(p1); // ToStringの自動生成
+            Console.WriteLine(p1 == p3); // true(プロパティ個々の同値性で判定)
+            (string fname, string lname, int age) = p2; // 分割代入
+            Console.WriteLine(fname);
+
+            Console.WriteLine("---------------------------");
+
+            // with式によるレコードの複製
+            // 元のレコードを複製+値を変更する仕組み(イミュータブルなまま変更したいとき)
+            Person p1with = p1 with { Age = 33 };
+            Console.WriteLine(p1with.Age);// 33
+            Console.WriteLine(p1.Age); // 22
+        }
+    }
+}
---
+++ SelfCSharp9_4/HiroshiHara/Chapter9/RecordTypeLearn/Record/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfCSharp9_4.HiroshiHara.Chapter9.RecordTypeLearn.Record
+{
+    // ★レコード型
+    // データの集合だけを扱うクラス
+    // 1.既定でイミュータブル
+    // 2.定型的なメンバーが自動生成
+    // 3.メンバーの追加は自由
+    internal record Person(string FirstName, string Lastname, int Age);
+    // 名前の後ろにプロパティを列挙する(プライマリコンストラクタ)
+    // 1.メンバ(get, initプロパティ)、及びコンストラクタを宣言したことと同義
+    // (=プロパティは初期化以外のタイミングでは変更不可)
+    
+}

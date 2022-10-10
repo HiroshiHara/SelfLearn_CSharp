3b62648c696d6ab13f2678d821549a2b46d6c7ee
HiroshiHara <mrbob403@gmail.com>
2022-10-10 14:57:04 +0900

    progress

---
+++ SelfCSharp10_1/HiroshiHara/Chapter10/DelegateLearn/Delegate/DelegateBasic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfCSharp10_1.HiroshiHara.Chapter10.DelegateLearn.Delegate
+{
+    // ★デリゲート宣言
+    // delegate 戻り値の型 デリゲート名(引数)
+    // Processデリゲートには、戻り値がvoidでstring引数を1つ受け取るメソッドを渡せる
+    // 本質は、メソッドの処理をシグニチャと切り分けられること
+    // 同じシグニチャに対し、状況次第で複数の処理を用意できる
+    delegate void Process(string str);
+    delegate void OutputProcess(string str);
+}
--- SelfCSharp10_1/HiroshiHara/Chapter10/DelegateLearn/Main/Program.cs
+++ SelfCSharp10_1/HiroshiHara/Chapter10/DelegateLearn/Main/Program.cs
@@ -1,10 +1,46 @@
-﻿namespace SelfCSharp10_1.HiroshiHara.Chapter10.DelegateLearn.Main
+﻿using SelfCSharp10_1.HiroshiHara.Chapter10.DelegateLearn.Delegate;
+
+namespace SelfCSharp10_1.HiroshiHara.Chapter10.DelegateLearn.Main
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            Process p = new Process(Run);
+            // Process p = Runでも可
+            p("Runメソッド");
+            string[] ary = { "aaa", "bbb", "ccc" };
+            OutputProcess op1 = AddBracket;
+            ArrayWalk(ary, op1);
+            OutputProcess op2 = AddCanma;
+            ArrayWalk(ary, op2);
+        }
 
+        public static void Run(string s)
+        {
+            Console.WriteLine($"{s}を実行");
+        }
+
+        // 第二引数にデリゲートを受け取る(具体的な処理内容を受け取る)
+        // 「配列を走査する」部分だけを定義して、走査で何をするかを移譲(delegate)する
+        public static void ArrayWalk(string[] ary, OutputProcess process)
+        {
+            foreach (var val in ary)
+            {
+                // デリゲートの実行
+                process(val);
+            }
+        }
+
+        // OutputProcessに対応した処理
+        static void AddBracket(string str)
+        {
+            Console.WriteLine($"[{str}]");
+        }
+
+        static void AddCanma(string str)
+        {
+            Console.WriteLine($"{str},");
         }
     }
 }

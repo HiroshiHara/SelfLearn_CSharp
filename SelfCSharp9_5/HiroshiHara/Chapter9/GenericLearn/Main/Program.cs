d00119868437a099de470de19c7703acedb098e0
HiroshiHara <mrbob403@gmail.com>
2022-10-10 14:03:17 +0900

    progress

---
+++ SelfCSharp9_5/HiroshiHara/Chapter9/GenericLearn/Main/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SelfCSharp9_5.HiroshiHara.Chapter9.GenericLearn.Main
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}

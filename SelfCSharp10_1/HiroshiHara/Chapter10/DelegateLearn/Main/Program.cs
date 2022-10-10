a2d30f1dbd99d53a252420b46d7d9f505a4b090f
HiroshiHara <mrbob403@gmail.com>
2022-10-10 14:32:49 +0900

    progress

---
+++ SelfCSharp10_1/HiroshiHara/Chapter10/DelegateLearn/Main/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SelfCSharp10_1.HiroshiHara.Chapter10.DelegateLearn.Main
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}

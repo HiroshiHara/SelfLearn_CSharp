c02d99616cdf05f86447e68efd8ecd9413c8ec2b
HiroshiHara <mrbob403@gmail.com>
2022-10-11 13:15:12 +0900

    progress

--- SelfCSharp10_2/HiroshiHara/Chapter10/LinqLearn/Main/Program.cs
+++ SelfCSharp10_2/HiroshiHara/Chapter10/LinqLearn/Main/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SelfCSharp10_2.HiroshiHara.Chapter10.LinqLearn.Tables;
 using SelfCSharp10_2.HiroshiHara.Chapter10.LinqLearn.Tables.Sub;
 
 namespace SelfCSharp10_2.HiroshiHara.Chapter10.LinqLearn.Main
@@ -11,7 +12,24 @@ namespace SelfCSharp10_2.HiroshiHara.Chapter10.LinqLearn.Main
     {
         public static void Main(string[] args)
         {
-            
+            // ★クエリ構文
+            // from, where, selectの順で記載
+            // ・from... エイリアス(in)が必須、in句に指定するものはIEnumerable/IEnumerable<T>インターフェースの実装ならなんでもOK
+            var bookList1 = from b in AppTables.Books
+                            where b.Price <= 3000
+                            select b.Title;
+            bookList1.ToList().ForEach(b => Console.Write($"{b} "));
+            Console.WriteLine("\r\n---------------------------");
+
+            // 複数のプロパティを取得
+            // (select句にカンマ区切りではNG)
+            var bookList2 = from b in AppTables.Books
+                            where b.Publisher == "翔泳社"
+                            select new { Title = b.Title, Price = b.Price };
+            bookList2.ToList().ForEach(b => Console.Write($"{b} "));
+            Console.WriteLine("\r\n---------------------------");
+
+
 
         }
     }
